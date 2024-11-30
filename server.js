d795b0e38a75c083fb0656f6c2fb84ad2f056ffa
awais-ur-rehman <awaisjarral37@gmail.com>
2024-11-30 20:52:43 +0500

    updated

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.post("/submit", (req, res) => {
+  const name = req.body.name;
+  res.send(`<h1>Hello, ${name}!</h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
