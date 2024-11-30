374022c6bccee656fb59a830d540602511a25bd9
Awais Ur Rehman <75811924+awais-ur-rehman@users.noreply.github.com>
2024-11-30 23:40:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
